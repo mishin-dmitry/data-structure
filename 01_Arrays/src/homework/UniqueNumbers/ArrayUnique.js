eefcda06c848802a855e1c676a58ec618501ab02
Dmitry <dmishin.dev@gmail.com>
2021-04-20 08:47:21 +0300

    made task 3

--- 01_Arrays/src/homework/UniqueNumbers/ArrayUnique.js
+++ 01_Arrays/src/homework/UniqueNumbers/ArrayUnique.js
@@ -1,9 +1,21 @@
 function lettersLearnedToday(word) {
-  return new Array(); // Please implement
+  return [...new Set(word.split(''))].join('');
 }
 
 function avoidJailDueToTaxFraud(report) {
-  return -1; // Please implement
+  let notUniqueValue = -1;
+  const flatReport = report.flat();
+
+  for (let i = 0; i < flatReport.length; i++) {
+    const firstIndexOfEl = flatReport.indexOf(flatReport[i]);
+
+    if (firstIndexOfEl !== i) {
+      notUniqueValue = flatReport[firstIndexOfEl];
+      break;
+    }
+  }
+
+  return notUniqueValue;
 }
 
 module.exports = {
