5326538e5342bd9042d04309a1a8b66770874069
Dmitry <dmishin.dev@gmail.com>
2021-04-20 09:41:17 +0300

    made task 4

--- 01_Arrays/src/homework/UniqueSorted/ArraySorted.js
+++ 01_Arrays/src/homework/UniqueSorted/ArraySorted.js
@@ -1,9 +1,15 @@
 const { printPhoneInfo, printCurrencyInfo } = require("./Printer");
 
 function groupAndPrint(phoneNumbers) {
-  // Please implement
-  // Use printPhoneInfo() method
-  // E.g. printPhoneInfo(+79000000000L, 22);
+  const numberCount = phoneNumbers.reduce((acc, cur) =>  {
+    acc[cur] = acc[cur] ? acc[cur] + 1 : 1;
+    return acc;
+  }, {});
+
+  const entries = Object.entries(numberCount);
+  entries.forEach(([number, count]) => {
+    printPhoneInfo(number, count);
+  });
 }
 
 function cryptoCurrencyAnalysis(fileContents) {
@@ -13,9 +19,15 @@ function cryptoCurrencyAnalysis(fileContents) {
     printCurrencyInfo(element.name, element.amount);
   });
 
-  // Please implement
-  // Use printCurrencyInfo() method
-  // E.g. printCurrencyInfo("BTC", 666.0);
+  const dataByCurrency = fileContents.reduce((acc, { name, amount }) => {
+    acc[name] = (acc[name] ?? 0) + amount;
+    return acc;
+  }, {});
+
+  const currencyEntries = Object.entries(dataByCurrency);
+  currencyEntries.forEach(([name, amount]) => {
+    printCurrencyInfo(name, amount);
+  });
 }
 
 module.exports = {
