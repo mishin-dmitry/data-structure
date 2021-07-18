423ee1d18932def3506341ea9bbbcb455e4978ce
d.mishin <d.mishin@MAC571RDS.local>
2021-07-18 11:40:59 +0300

    working on binary search hw

--- 02_BinarySearch/homework/BinarySearch/BinarySearch.js
+++ 02_BinarySearch/homework/BinarySearch/BinarySearch.js
@@ -1,5 +1,21 @@
 function doIKnowThisLanguage(languagesList, language) {
-  return false; // please implement
+  let left = 0;
+  let right = languagesList.length - 1;
+  let middle = Math.floor((left + right) / 2)
+
+  while (left <= right) {
+    if (languagesList[middle] < language) {
+      left = middle + 1
+    } else if (languagesList[middle] > language) {
+      right = middle - 1
+    } else {
+      return true
+    }
+
+    middle = Math.floor((left + right) / 2)
+  }
+
+  return false
 }
 
 class DatingUser {
@@ -9,8 +25,52 @@ class DatingUser {
   }
 }
 
+const findValue = (array, target) => {
+  let left = 0
+  let right = array.length - 1
+  let middle = Math.floor((left + right) / 2)
+
+  while (left <= right) {
+    if (array[middle].IQ < target) {
+      left = middle + 1
+    } else if (array[middle].IQ > target) {
+      right = middle -1
+    } else {
+      return array.slice(middle, middle + 1)
+    }
+
+    middle = Math.floor((left + right) / 2)
+  }
+
+  return []
+}
+
 function findUsers(usersSortedByIQ, lowerIQBound, professorIQ) {
-  return new Array(); // please implement
+  if (lowerIQBound === professorIQ) {
+    return findValue(usersSortedByIQ, lowerIQBound)
+  }
+
+  const targetDiff = professorIQ - lowerIQBound
+
+  let left = 0
+  let right = usersSortedByIQ.length - 1
+  let middle = Math.floor((left + right) / 2)
+  let currentDiff = usersSortedByIQ[right].IQ - usersSortedByIQ[left].IQ
+
+  while (currentDiff > targetDiff && left < right) {
+    if (usersSortedByIQ[middle].IQ > lowerIQBound) {
+      left = middle
+    }
+
+    if (usersSortedByIQ[middle].IQ > professorIQ) {
+      right = middle - 1
+    }
+
+    middle = Math.floor((left + right) / 2)
+    currentDiff = usersSortedByIQ[right].IQ - usersSortedByIQ[left].IQ
+  }
+
+  return usersSortedByIQ.slice(left, right + 1);
 }
 
 function findPhoneNumber(sortedPhoneNumbers, search) {
