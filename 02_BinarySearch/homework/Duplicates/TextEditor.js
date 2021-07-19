95c5857de6f42fd2230b46fc7803c196cb9bef5f
d.mishin <d.mishin@MAC571RDS.local>
2021-07-19 21:17:23 +0300

    made duplicate first hw

--- 02_BinarySearch/homework/Duplicates/TextEditor.js
+++ 02_BinarySearch/homework/Duplicates/TextEditor.js
@@ -13,6 +13,16 @@ class TextEditor {
   // Например если textLength = 3, и text = {‘ш’,’к’,’я’, … еще 252 символа ‘пробел’ .. }
 
   insertCharacterAt(position, c) {
+    for (let i=0; i <= this.textLength; i++) {
+      if (i === position) {
+        for (let k = this.textLength; k >= position; k--) {
+          this.text[k + 1] = this.text[k]
+        }
+
+        this.text[position] = c
+        this.textLength++
+      }
+    }
     // Эта функция должна вставлять символ в строку на позиции position
     // Например когда пользователь напечатал “При|ет мир!” (этот текст у нас в переменной text)
     // и его курсор находится между буквами “и” и “е”, и он нажимает “в”.
@@ -20,6 +30,15 @@ class TextEditor {
   }
 
   backspace(position) {
+    for (let i=0; i < this.textLength; i++) {
+      if (i === position) {
+        for (let k = position; k <= this.textLength; k++) {
+          this.text[k - 1] = this.text[k]
+        }
+
+        this.textLength--
+      }
+    }
     // “Привет Мм|ир!” => “Привет Мир!”
   }
 }
