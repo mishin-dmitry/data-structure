06f46748f251284c75d42dc3a810a1a0e7219982
d.mishin <d.mishin@MAC571RDS.local>
2021-08-05 09:33:28 +0300

    refactor: removed superflous loops

--- 02_BinarySearch/homework/Duplicates/TextEditor.js
+++ 02_BinarySearch/homework/Duplicates/TextEditor.js
@@ -13,16 +13,12 @@ class TextEditor {
   // Например если textLength = 3, и text = {‘ш’,’к’,’я’, … еще 252 символа ‘пробел’ .. }
 
   insertCharacterAt(position, c) {
-    for (let i=0; i <= this.textLength; i++) {
-      if (i === position) {
-        for (let k = this.textLength; k >= position; k--) {
-          this.text[k + 1] = this.text[k]
-        }
-
-        this.text[position] = c
-        this.textLength++
-      }
+    for (let i=this.textLength; i >= position; i--) {
+      this.text[i + 1] = this.text[i]
     }
+    
+    this.text[position] = c
+    this.textLength++
     // Эта функция должна вставлять символ в строку на позиции position
     // Например когда пользователь напечатал “При|ет мир!” (этот текст у нас в переменной text)
     // и его курсор находится между буквами “и” и “е”, и он нажимает “в”.
@@ -30,16 +26,11 @@ class TextEditor {
   }
 
   backspace(position) {
-    for (let i=0; i < this.textLength; i++) {
-      if (i === position) {
-        for (let k = position; k <= this.textLength; k++) {
-          this.text[k - 1] = this.text[k]
-        }
-
-        this.textLength--
-      }
+    for (let i=position; i <= this.textLength; i++) {
+      this.text[i - 1] = this.text[i]
     }
-    // “Привет Мм|ир!” => “Привет Мир!”
+
+    this.textLength--
   }
 }
 
