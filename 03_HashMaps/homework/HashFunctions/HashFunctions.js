1f078ac210a7262ad6b67be1ceda54ba2f7b5907
d.mishin <d.mishin@rambler-co.ru>
2021-09-20 13:41:22 +0300

    feat: added 2 homeworks

--- 03_HashMaps/homework/HashFunctions/HashFunctions.js
+++ 03_HashMaps/homework/HashFunctions/HashFunctions.js
@@ -1,9 +1,15 @@
 function hashString(input) {
-  return -1; // Please implement
+  return input.split("").reduce((acc, cur) => (
+    acc += cur.charCodeAt(0)
+  ), 0)
 }
 
 function hashInt(input) {
-  return -1; // Please implement
+  const result = input.split("").reduce((acc, cur) => (
+    acc = (typeof acc === 'string' ? +acc : acc) * +cur
+  ))
+
+  return `${input.split("").join('*')}=${result}`
 }
 
 class Student {
@@ -14,5 +20,8 @@ class Student {
 }
 
 function hashStudent(input) {
-  return -1; // Please implement
+  const nameHash = hashString(input.name)
+  const ageHash = hashInt(input.age)
+  
+  return hashString(nameHash + ageHash)
 }
--- 03_HashMaps/homework/HashMapsAreFast/HashMapsAreFast.js
+++ 03_HashMaps/homework/HashMapsAreFast/HashMapsAreFast.js
@@ -1,8 +1,83 @@
-function getUniqueNumbers(inputArray) {
-  return []; // please implement
+class HashMap {
+  constructor() {
+    this.values = new Array(8)
+    this.count = 0
+    this.size = 8
+  }
+
+  add(value) {
+    const index = this.findGoodIndex(value)
+
+    this.values[index] = value
+
+    if (this.size === this.count) {
+      this.resize(this.count * 2)
+    }
+  }
+
+  resize(newSize) {
+    const newValues = new Array(newSize);
+
+    for (let i = 0; i < this.count; i++) {
+      const index = this.values[i] % newSize
+      newValues[index] = this.values[i]
+    }
+
+    this.values = newValues
+    this.size = newSize
+  }
+
+  hasValue(value) {
+    const index = value % this.size
+    const existValue = this.values[index]
+
+    return existValue && existValue === value
+  }
+
+  getValues() {
+    return this.values.filter(i => !!i);
+  }
+
+  findGoodIndex(searchValue) {
+    const index = searchValue % this.size
+
+    for (let i = 0; i < this.size; i++) {
+      const tmpIndex = (index + i) % this.size;
+      const existValue = this.values[tmpIndex]
+
+      if (!existValue || existValue === searchValue) {
+        if (!existValue) {
+          this.count++
+        }
+
+        return tmpIndex;
+      }
+    }
+  }
 }
 
+function getUniqueNumbers(inputArray) {
+  const hashMap = new HashMap();
+
+  for (let i = 0; i < inputArray.length; i++) {
+    hashMap.add(inputArray[i])
+  }
+
+  return hashMap.getValues()
+}
 
 function isThereTwoNumbers(numbers, X) {
-    return false; // please implement
+  const hashMap = new HashMap();
+
+  for (let i = 0; i < numbers.length; i++) {
+    const difference = X - numbers[i]
+
+    if (hashMap.hasValue(numbers[i])) {
+      return true
+    }
+
+    hashMap.add(difference)
+  }
+
+  return false;
 }
